25024e8073217a1aa62c63804344c9460332d123
laurynas kapacinskas <laurynas.kapacinskas@gmail.com>
2021-09-23 10:50:10 +0300

    Initial Commit

---
+++ DogFacto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogFacto
+//
+//  Created by Laurynas Kapacinskas on 2021-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
